862e945f3606dd0cac79ad3c5f3930932a6373ac
Nadejda Barbarov <nadejda.barbarov@isa.utm.com>
2024-10-26 14:12:28 +0300

    Choose a book type

--- src/BookFactory.java
+++ src/BookFactory.java
@@ -1,2 +1,15 @@
+// BookFactory class
 public class BookFactory {
+    public static Book createBook(String bookType, String title, String author, double price, Object... args) {
+        switch (bookType.toLowerCase()) {
+            case "ebook":
+                return new Ebook(title, author, price, (String) args[0]);
+            case "paperback":
+                return new Paperback(title, author, price, (Integer) args[0]);
+            case "hardcover":
+                return new Hardcover(title, author, price, (Integer) args[0], (String) args[1]);
+            default:
+                throw new IllegalArgumentException("Unknown book type");
+        }
+    }
 }
