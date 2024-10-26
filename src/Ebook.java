4ddd1b35596412bcd39387ad11b7b0bc205e3dfb
Nadejda Barbarov <nadejda.barbarov@isa.utm.com>
2024-10-26 14:02:50 +0300

    Different types of books

--- src/Ebook.java
+++ src/Ebook.java
@@ -1,2 +1,20 @@
-public class Ebook {
+// Ebook class
+public class Ebook implements Book {
+    private String title;
+    private String author;
+    private double price;
+    private String fileFormat;
+
+    public Ebook(String title, String author, double price, String fileFormat) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        this.fileFormat = fileFormat;
+    }
+
+    // Getters
+    public String getTitle() { return title; }
+    public String getAuthor() { return author; }
+    public double getPrice() { return price; }
+    public String getFileFormat() { return fileFormat; }
 }
--- src/Hardcover.java
+++ src/Hardcover.java
@@ -1,2 +1,23 @@
-public class Hardcover {
+// Hardcover class
+public class Hardcover implements Book {
+    private String title;
+    private String author;
+    private double price;
+    private int pages;
+    private String weight;
+
+    public Hardcover(String title, String author, double price, int pages, String weight) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        this.pages = pages;
+        this.weight = weight;
+    }
+
+    // Getters
+    public String getTitle() { return title; }
+    public String getAuthor() { return author; }
+    public double getPrice() { return price; }
+    public int getPages() { return pages; }
+    public String getWeight() { return weight; }
 }
