6db42ae646153efa7f15428a49c120140212c28b
Nadejda Barbarov <nadejda.barbarov@isa.utm.com>
2024-11-24 11:30:32 +0200

    Facade modified

--- src/domain/utilities/BookstoreFacade.java
+++ src/domain/utilities/BookstoreFacade.java
@@ -10,43 +10,53 @@ public class BookstoreFacade {
     private final BookFactory bookFactory;
     private final Payment paymentProcessor;
     private final OrderBuilder orderBuilder;
+    private final CommandInvoker commandInvoker;
 
     public BookstoreFacade(BookFactory factory, Payment paymentProcessor) {
         this.bookFactory = factory;
         this.paymentProcessor = paymentProcessor;
         this.orderBuilder = new OrderBuilder();
+        this.commandInvoker = new CommandInvoker();
     }
 
     public void addBookToCart(String type, String title, String author, double price, String formatOrWeight) {
-        // Check the book type and ensure the correct number of arguments are passed
         if (type.equalsIgnoreCase("hardcover") && formatOrWeight == null) {
             throw new IllegalArgumentException("Weight must be provided for hardcover books.");
         }
 
         Book book = BookFactory.createBook(type, title, author, price, formatOrWeight);
-        orderBuilder.addBook(book);  // Add the book to the order using the builder
+        Command addBookCommand = new AddBookToCartCommand(orderBuilder, book);
+        commandInvoker.executeCommand(addBookCommand);
     }
 
-
     public void setShippingInfo(String shippingInfo) {
-        orderBuilder.setShippingInfo(shippingInfo);  // Set shipping info
+        Command setShippingCommand = new SetShippingInfoCommand(orderBuilder, shippingInfo);
+        commandInvoker.executeCommand(setShippingCommand);
     }
 
     public void setPaymentMethod(String paymentMethod) {
-        orderBuilder.setPaymentMethod(paymentMethod);  // Set payment method
+        Command setPaymentMethodCommand = new SetPaymentMethodCommand(orderBuilder, paymentMethod);
+        commandInvoker.executeCommand(setPaymentMethodCommand);
     }
 
+
     public void checkout() {
-        Order order = orderBuilder.build();  // Build the order
-        paymentProcessor.process(order.getTotal());  // Process payment
-        System.out.println("Order processed with " + order.getPaymentMethod() + " and shipping to " + order.getShippingInfo());
+        Command checkoutCommand = new CheckoutCommand(orderBuilder, paymentProcessor);
+        commandInvoker.executeCommand(checkoutCommand);
     }
 
     public void viewCartDetails() {
-        Order order = orderBuilder.build();  // Build the order to view details
+        Order order = orderBuilder.build();
         for (Book book : order.getBooks()) {
             System.out.println(book.getTitle() + " - $" + book.getPrice());
         }
         System.out.println("Total: $" + order.getTotal());
+    }
+
+    public void viewCommandHistory() {
+        System.out.println("Command History:");
+        for (Command command : commandInvoker.getCommandHistory()) {
+            System.out.println(command.getClass().getSimpleName());
+        }
     }
 }
